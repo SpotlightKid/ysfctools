f4dc99275b7be6abb69309a2af9dffab6ade9c0e
Christopher Arndt <chris@chrisarndt.de>
2020-03-22 20:56:23 +0100

    Handle midnam file using 'UsesPatchNameList' references
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- midnam2xlsx/main.py
+++ midnam2xlsx/main.py
@@ -8,6 +8,7 @@
 import argparse
 import logging
 import sys
+from collections import namedtuple
 import xml.etree.ElementTree as etree
 
 from openpyxl import Workbook
@@ -16,7 +17,7 @@ from openpyxl.utils import get_column_letter
 from .styles import colhead, numcell, txtcell
 
 
-log = logging.getLogger(__file__)
+log = logging.getLogger("midnam2xlsx")
 
 COLUMNS_ALL = {
     "A": ("Bank", 8, colhead, numcell),
@@ -53,71 +54,97 @@ CATEGORIES = {
 }
 
 
+PatchBank = namedtuple("PatchBank", ("name", "msb", "lsb", "patchlist"))
+Patch = namedtuple("Patch", ("name", "category", "program"))
+
+
 def sanitize(s):
     for repl in r"\/*?:[]":
         s = s.replace(repl, " ")
     return s
 
 
+def parse_patchnamelist(patchnamelist):
+    patchlist = []
+
+    for pnum, patch in enumerate(patchnamelist.findall("./Patch"), start=1):
+        pnumber = patch.get("Number", pnum)
+        pname = patch.get("Name", "<unnamed patch #{}".format(patch.get("Number", pnum)))
+
+        # If name is the same as the patch number, skip this patch
+        try:
+            if int(pnumber) == int(pname):
+                continue
+        except (TypeError, ValueError):
+            pass
+
+        try:
+            program = int(patch.get("ProgramChange"))
+        except:
+            program = ""
+
+        try:
+            category, pname = pname.split(":", 1)
+        except:
+            category = ""
+        else:
+            category = CATEGORIES.get(category, category)
+
+        patchlist.append(Patch(pname, category, program))
+
+    return patchlist
+
+
 def parse_midnam(fileobj):
     tree = etree.parse(fileobj)
-    namesets = {}
+    root = tree.getroot()
 
-    for nsnum, nameset in enumerate(tree.findall("*/ChannelNameSet")):
-        nsname = nameset.get("Name", "<unnamed set #{}>".format(nsnum + 1))
+    if root.tag != "MIDINameDocument":
+        raise ValueError("Unrecognized document type '%s'." % root.tag)
+
+    patchlists = {}
+    for plnum, patchnamelist in enumerate(root.findall("./MasterDeviceNames/PatchNameList"), start=1):
+        plname = patchnamelist.get("Name", "<unnamed patchlist #{}>".format(plnum))
+        log.debug("Patch Name List: %s", plname)
+        patchlists[plname] = parse_patchnamelist(patchnamelist)
+
+    namesets = {}
+    for nsnum, nameset in enumerate(root.findall("./MasterDeviceNames/ChannelNameSet"), start=1):
+        nsname = nameset.get("Name", "<unnamed set #{}>".format(nsnum))
         log.debug("Channel Name Set: %s", nsname)
 
-        patchbanks = {}
+        patchbanks = []
 
         for pbnum, patchbank in enumerate(nameset.findall("./PatchBank")):
             pbname = patchbank.get("Name", "<unnamed bank #{}>".format(pbnum + 1))
             log.debug("Patch Bank: %s", pbname)
 
-            msb = patchbank.findall("./MIDICommands/ControlChange[@Control='0']")
+            msb = patchbank.find("./MIDICommands/ControlChange[@Control='0']")
 
             try:
-                msb = int(msb[0].get("Value"))
+                msb = int(msb.get("Value"))
             except:
                 msb = ""
 
-            lsb = patchbank.findall("./MIDICommands/ControlChange[@Control='32']")
+            lsb = patchbank.find("./MIDICommands/ControlChange[@Control='32']")
 
             try:
-                lsb = int(lsb[0].get("Value"))
+                lsb = int(lsb.get("Value"))
             except:
                 lsb = ""
 
-            patches = []
+            patchlist_ref = patchbank.find("./UsesPatchNameList")
 
-            for pnum, patch in enumerate(patchbank.findall("./PatchNameList/Patch")):
-                pnumber = patch.get("Number")
-                pname = patch.get(
-                    "Name", "<unnamed patch #{}".format(patch.get("Number", pnum + 1))
-                )
-
-                # If name is the same as the patch number, skip this patch
+            if patchlist_ref is not None:
+                plname = patchlist_ref.get("Name")
                 try:
-                    if int(pnumber) == int(pname):
-                        continue
-                except (TypeError, ValueError):
-                    pass
-
-                try:
-                    pc = int(patch.get("ProgramChange"))
-                except:
-                    pc = ""
-
-                try:
-                    category, pname = pname.split(":", 1)
-                except:
-                    category = ""
-                else:
-                    category = CATEGORIES.get(category, category)
-
-                patches.append([pbname, msb, lsb, pc, category, pname])
-
-            if patches:
-                patchbanks[pbname] = patches
+                    patchbanks.append(PatchBank(pbname, msb, lsb, patchlists[plname]))
+                except KeyError:
+                    log.warning("Patch name list '%s' referenced by Patch bank '%s' not found.",
+                                plname, pbname)
+            else:
+                patchlist = parse_patchnamelist(patchbank.find("./PatchNameList"))
+                patchbanks.append(PatchBank(pbname, msb, lsb, patchlist))
 
         if patchbanks:
             namesets[nsname] = patchbanks
@@ -125,7 +152,7 @@ def parse_midnam(fileobj):
     return namesets
 
 
-def write_xlsx(nsname, nameset):
+def write_xlsx(xlsxname, nameset):
     wb = Workbook()
     wb.add_named_style(colhead)
 
@@ -142,8 +169,11 @@ def write_xlsx(nsname, nameset):
         ws_all.column_dimensions[col].width = width
 
     # Create one work sheet for each patch bank
-    for pbnum, (pbname, patchbank) in enumerate(nameset.items()):
-        ws = wb.create_sheet(title=sanitize(pbname))
+    for pbnum, patchbank in enumerate(nameset):
+        if not patchbank.patchlist:
+            continue
+
+        ws = wb.create_sheet(title=sanitize(patchbank.name))
         ws.freeze_panes = "A2"
 
         # Set column header styles for patch bank worksheet
@@ -152,16 +182,23 @@ def write_xlsx(nsname, nameset):
             ws[col + "1"].style = headstyle
             ws.column_dimensions[col].width = width
 
-        for row, patch in enumerate(patchbank, start=2):
-            if pbname != "GM":
-                ws_all.append(patch)
+        for row, patch in enumerate(patchbank.patchlist, start=2):
+            if patchbank.name != "GM":
+                ws_all.append((
+                    patchbank.name,
+                    patchbank.msb,
+                    patchbank.lsb,
+                    patch.program,
+                    patch.category,
+                    patch.name
+                ))
                 ws_all_row += 1
 
                 # Set cell styles for row in 'All' work sheet
                 for column in COLUMNS_ALL:
                     ws_all["%s%i" % (column, ws_all_row)].style = COLUMNS_ALL[column][3]
 
-            ws.append(patch[3:])
+            ws.append((patch.program, patch.category, patch.name))
 
             # Set cell styles for row in patch bank work sheet
             for column in COLUMNS_BANK:
@@ -170,20 +207,12 @@ def write_xlsx(nsname, nameset):
     # Set auto filter for 'All' work sheet
     ws_all.auto_filter.ref = ws_all.dimensions
 
-    # Generate filename from nameset name
-    basename = sanitize(nsname).strip()
-
-    if basename:
-        xlsxname = basename + ".xlsx"
-    else:
-        xlsxname = "nameset-%02i.xlsx" % (nsnum + 1)
-
     log.info("Writing '%s'...", xlsxname)
     wb.save(xlsxname)
 
 
 def main(args=None):
-    ap = argparse.ArgumentParser(description=__doc__.splitlines()[0])
+    ap = argparse.ArgumentParser(prog="midnam2xlsx", description=__doc__.splitlines()[0])
     ap.add_argument("-v", "--verbose", action="store_true", help="Be verbose")
     ap.add_argument("midnam", type=open, help="MIDNAM input file")
 
@@ -197,8 +226,16 @@ def main(args=None):
     with args.midnam:
         namesets = parse_midnam(args.midnam)
 
-    for nsname, nameset in namesets.items():
-        write_xlsx(nsname, nameset)
+    for nsnum, (nsname, nameset) in enumerate(namesets.items(), start=1):
+        # Generate filename from nameset name
+        basename = sanitize(nsname).strip()
+
+        if basename:
+            xlsxname = basename + ".xlsx"
+        else:
+            xlsxname = "nameset-%02i.xlsx" % nsmum
+
+        write_xlsx(xlsxname, nameset)
 
 
 if __name__ == "__main__":
